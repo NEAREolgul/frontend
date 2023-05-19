a3119c8416c0f0b91bcaeadc786d26c3dcda28b0
simonieee <75366051+simonieee@users.noreply.github.com>
2023-05-20 00:13:53 +0900

    [UPDATE] migration
    - next -> jsx 마이그레이션 중

--- src/Routes/pages/Main/MainPresenter.jsx
+++ src/Routes/pages/Main/MainPresenter.jsx
@@ -1,5 +1,15 @@
+import Blog from "../../../Components/Blog";
+import Carousel from "../../../Components/Carousel";
+import Inspiration from "../../../Components/Inspiration";
+
 const MainPresenter = () => {
-  return <div>MainPresenter</div>;
+  return (
+    <div>
+      <Blog />
+      <Carousel />
+      <Inspiration />
+    </div>
+  );
 };
 
 export default MainPresenter;
