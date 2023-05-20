baeecde033be966c1eb27d531281e07ff5d49e68
simonieee <75366051+simonieee@users.noreply.github.com>
2023-05-21 07:39:52 +0900

    [UPDATE] For Vedio

--- src/Routes/pages/BuyNFT/BuyNFTContainer.jsx
+++ src/Routes/pages/BuyNFT/BuyNFTContainer.jsx
@@ -1,10 +1,10 @@
-import BuyNFTPresenter from './BuyNFTPresenter';
-import { useState } from 'react';
-import PostAuthor01 from '../../../assets/images/blog-author-01.jpg';
-import PostAuthor02 from '../../../assets/images/blog-author-02.jpg';
-import PostAuthor03 from '../../../assets/images/blog-author-03.jpg';
-import PostAuthor04 from '../../../assets/images/blog-author-04.jpg';
-import PostAuthor05 from '../../../assets/images/blog-author-05.jpg';
+import BuyNFTPresenter from "./BuyNFTPresenter";
+import { useState } from "react";
+import PostAuthor01 from "../../../assets/images/blog-author-01.jpg";
+import PostAuthor02 from "../../../assets/images/blog-author-02.jpg";
+import PostAuthor03 from "../../../assets/images/blog-author-03.jpg";
+import PostAuthor04 from "../../../assets/images/blog-author-04.jpg";
+import PostAuthor05 from "../../../assets/images/blog-author-05.jpg";
 
 const BuyNFTContainer = (props) => {
   /* Router */
@@ -12,31 +12,36 @@ const BuyNFTContainer = (props) => {
   const { isSignedIn } = props;
   const [buyList, setBuyList] = useState([
     {
-      price: '8',
-      user_nm: 'Simon',
+      price: "8",
+      user_nm: "Simon",
       authSrc: PostAuthor03,
     },
     {
-      price: '19',
-      user_nm: 'KyeonWoo',
+      price: "19",
+      user_nm: "KyeonWoo",
       authSrc: PostAuthor05,
     },
     {
-      price: '26',
-      user_nm: 'BoGeum',
+      price: "26",
+      user_nm: "BoGeum",
       authSrc: PostAuthor01,
     },
   ]);
   const [bidList, setBidList] = useState([
     {
-      price: '38',
-      user_nm: 'Simon',
+      price: "38",
+      user_nm: "Simon",
       authSrc: PostAuthor03,
     },
     {
-      price: '58',
-      user_nm: 'KyeongWoo',
+      price: "58",
+      user_nm: "KyeongWoo",
       authSrc: PostAuthor05,
+    },
+    {
+      price: "70",
+      user_nm: "HanHo",
+      authSrc: PostAuthor04,
     },
   ]);
 
